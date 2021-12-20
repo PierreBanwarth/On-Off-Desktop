6cafa9e54bc09c5f165e19e2552c212b10e8044e
Banwarth Pierre <pi.banwarth@gmail.com>
2021-12-20 21:33:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import RPi.GPIO as GPIO #Importe la bibliothèque pour contrôler les GPIOs
+from gpiozero import Button
 
-GPIO.setmode(GPIO.BCM) #Définit le mode de numérotation (Board)
+GPIO.setmode(GPIO.GPIO) #Définit le mode de numérotation (Board)
 GPIO.setwarnings(False) #On désactive les messages d'alerte
 
-LED = 26 #Définit le numéro du port GPIO qui alimente la led
+SWITCH = 26 #Définit le numéro du port GPIO qui alimente la led
 
-GPIO.setup(LED, GPIO.OUT) #Active le contrôle du GPIO
-
-state = GPIO.input(LED) #Lit l'état actuel du GPIO, vrai si allumé, faux si éteint
-print(state)
+input = GPIO.input(SWITCH)
+print(input)
