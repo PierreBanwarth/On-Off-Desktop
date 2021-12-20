24e96479211c1a3f9f7874958c5c7b6fa8e2317d
Banwarth Pierre <pi.banwarth@gmail.com>
2021-12-20 21:22:34 +0100

    first code test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO #Importe la bibliothèque pour contrôler les GPIOs
 GPIO.setmode(GPIO.BOARD) #Définit le mode de numérotation (Board)
 GPIO.setwarnings(False) #On désactive les messages d'alerte
 
-LED = 26s #Définit le numéro du port GPIO qui alimente la led
+LED = 26 #Définit le numéro du port GPIO qui alimente la led
 
 GPIO.setup(LED, GPIO.OUT) #Active le contrôle du GPIO
 
