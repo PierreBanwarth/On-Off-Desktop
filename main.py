4e208443a77057e7f6704dc3faabb5261c01a309
Banwarth Pierre <pi.banwarth@gmail.com>
2021-12-21 01:05:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import time
+import requests
+from wakeonlan import send_magic_packet
+from octorest import OctoRest
 
+
+macAdressTest = 'ff.ff.ff.ff.ff.ff'
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
@@ -20,3 +25,35 @@ while True: # Run forever
         print("low")
 
         time.sleep(0.5)
+
+
+# https://pypi.org/project/tinytuya/
+# pip install tinytuya
+# Online management of on/off switchs for 3D printer and Light (futur for screens too)
+
+
+
+
+
+# shutdown computer ok
+def shutdownComputer():
+    response = requests.get("http://remote-host-name:5001/secret/")
+
+def wakeOnLanComputer(macAdress):
+    send_magic_packet(macAdress)
+
+# pip install octorest
+def get_printer_info():
+    try:
+        client = OctoRest(url="http://octopi.local", apikey="YouShallNotPass")
+        message = ""
+        message += str(client.version) + "\n"
+        message += str(client.job_info()) + "\n"
+        printing = client.printer()['state']['flags']['printing']
+        if printing:
+            message += "Currently printing!\n"
+        else:
+            message += "Not currently printing...\n"
+        return message
+    except Exception as e:
+        print(e)
